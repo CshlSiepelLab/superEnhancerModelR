ff22763198b570473f65079409deae677cf56254
Noah Dukler <ned36@cornell.edu>
2018-03-30 10:43:11 -0400

    Documentation updates and improved plotting

--- R/plotResiduals.R
+++ R/plotResiduals.R
@@ -1,17 +1,17 @@
-methods::setGeneric("plotModel", function(x,...) {
-  standardGeneric("plotModel")
+methods::setGeneric("plotResiduals", function(x,...) {
+  standardGeneric("plotResiduals")
 })
 
-#' Plots Model
+#' Plots REsidual
 #'
-#' Plots data and model prediction
+#' Plots model residuals
 #' @param x enhancerDataObject
 #' @name optimMod
 #' @include enhancerDataObject-class.R
 #' @examples
 #'
 #' @export
-methods::setMethod("plotModel", signature(x = "enhancerDataObject"),plotModel <- function(x){
+methods::setMethod("plotResiduals", signature(x = "enhancerDataObject"),plotModel <- function(x){
   ## Get names of active enhancers for each experiment
   indiv.enhancers=grep(x = colnames(x@designMatrix),pattern = ")|:",invert = TRUE)
   rname=apply(x@designMatrix,1, function(z) paste(colnames(x@designMatrix)[indiv.enhancers][as.logical(z[indiv.enhancers])],collapse = "/"))
@@ -21,9 +21,12 @@ methods::setMethod("plotModel", signature(x = "enhancerDataObject"),plotModel <-
   temp=data.frame(rname=rname,nenh=rowSums(x@designMatrix),stringsAsFactors = FALSE)
   lvls=unique(with(temp,temp[order(nenh,rname),])$rname)
 
+  ## Get predicted expression
+  predE=predictExpression(x)
+
   ## Get activity values for actual oberservations
   act=computeActivity(x)
-  real=data.frame(activity=act,observed=x@expressionData,enhancers=rname,stringsAsFactors = FALSE)
+  real=data.frame(activity=act,residual=x@expressionData-predE,enhancers=rname,stringsAsFactors = FALSE)
   real$enhancers=factor(real$enhancers,levels=lvls)
 
   ## Compute expression values for intermediate activity to get smooth curve
@@ -31,17 +34,19 @@ methods::setMethod("plotModel", signature(x = "enhancerDataObject"),plotModel <-
   out=data.frame(activity=sim.act,expression=predictExpression(x,sim.act))
 
   err=errorIntervals(x,sim.act,quantiles = 0.1)
+  err$y=err$y-c(out$expression,rev(out$expression))
 
   cc <- scales::seq_gradient_pal("light blue", "blue", "Lab")(seq(0,0.5,length.out=length(unique(err$Quantile))))
 
   g=ggplot2::ggplot()+
     ggplot2::geom_polygon(data=err,ggplot2::aes(x=x,y=y,fill=Quantile),alpha=1)+
     ggplot2::scale_fill_manual(values=cc)+
-    ggplot2::geom_line(data=out,ggplot2::aes(activity,expression),color="black",size=2)+
-    ggplot2::geom_point(data=real,ggplot2::aes(activity,observed,color=enhancers))+
     ggplot2::theme_bw(base_size = 28)+
     ggplot2::xlab("Activity/-Energy")+
-    ggplot2::ylab("Expression")
+    ggplot2::ylab("Expression")+
+    ggplot2::geom_hline(yintercept = 0,linetype=3)+
+    ggplot2::geom_point(data=real,ggplot2::aes(activity,residual,color=enhancers))
+
 
   return(g)
 })
